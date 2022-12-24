8091762643d3ae5beca429b2ca4d54e23fe6be10
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-12-24 20:29:06 +0700

    Update side bar

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,75 +1,50 @@
-// ** Icon imports
-import Login from 'mdi-material-ui/Login'
-import Table from 'mdi-material-ui/Table'
-import CubeOutline from 'mdi-material-ui/CubeOutline'
+import Logout from 'mdi-material-ui/Logout'
 import HomeOutline from 'mdi-material-ui/HomeOutline'
-import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
 import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
-import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
 import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
-import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
-import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
+import ArchiveOutline from 'mdi-material-ui/ArchiveOutline'
+import FileDocumentOutline from 'mdi-material-ui/FileDocumentOutline'
 
 const navigation = () => {
   return [
     {
-      title: 'Dashboard',
+      title: 'Beranda',
       icon: HomeOutline,
       path: '/'
     },
     {
-      title: 'Account Settings',
-      icon: AccountCogOutline,
-      path: '/account-settings'
+      sectionTitle: 'Garansi'
     },
     {
-      sectionTitle: 'Pages'
+      title: 'Data Garansi',
+      icon: ArchiveOutline,
+      path: '/data-garansi'
     },
     {
-      title: 'Login',
-      icon: Login,
-      path: '/pages/login',
-      openInNewTab: true
+      title: 'Klaim Garansi',
+      icon: FileDocumentOutline,
+      path: '/klaim-garansi'
     },
     {
-      title: 'Register',
+      sectionTitle: 'Pengguna'
+    },
+    {
+      title: 'Data Pengguna',
       icon: AccountPlusOutline,
-      path: '/pages/register',
-      openInNewTab: true
+      path: '/data-pengguna'
     },
     {
-      title: 'Error',
-      icon: AlertCircleOutline,
-      path: '/pages/error',
-      openInNewTab: true
+      sectionTitle: 'Akun'
     },
     {
-      sectionTitle: 'User Interface'
+      title: 'Pengaturan Akun',
+      icon: AccountCogOutline,
+      path: '/pengaturan-akun'
     },
     {
-      title: 'Typography',
-      icon: FormatLetterCase,
-      path: '/typography'
-    },
-    {
-      title: 'Icons',
-      path: '/icons',
-      icon: GoogleCirclesExtended
-    },
-    {
-      title: 'Cards',
-      icon: CreditCardOutline,
-      path: '/cards'
-    },
-    {
-      title: 'Tables',
-      icon: Table,
-      path: '/tables'
-    },
-    {
-      icon: CubeOutline,
-      title: 'Form Layouts',
-      path: '/form-layouts'
+      title: 'Keluar',
+      icon: Logout,
+      path: '/keluar'
     }
   ]
 }
