365f088df51700a4ae1855767d04d22afbed3565
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-12-28 08:04:18 +0700

    Add action button DataGaransi page

--- src/pages/data-garansi/index.js
+++ src/pages/data-garansi/index.js
@@ -1,8 +1,12 @@
+import useMediaQuery from '@mui/material/useMediaQuery'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
 import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
 
 import Plus from 'mdi-material-ui/Plus'
+import Import from 'mdi-material-ui/Import'
+import Export from 'mdi-material-ui/Export'
 
 import { useGaransi } from 'src/hooks/useGaransi'
 import GaransiTable from 'src/views/data-garansi/GaransiTable'
@@ -17,6 +21,7 @@ const createData = (id, name, startDate, endDate) => {
 }
 
 const DataGaransi = () => {
+  const fullWidth = useMediaQuery(theme => theme.breakpoints.down('md'))
   const { data } = useGaransi()
 
   const rows = data.map(row => {
@@ -26,9 +31,17 @@ const DataGaransi = () => {
   return (
     <Grid container spacing={6}>
       <Grid item xs={12}>
-        <Button variant='contained' startIcon={<Plus />}>
-          Tambah Data Garansi
-        </Button>
+        <Stack gap={2} direction={fullWidth ? 'column' : 'row'}>
+          <Button variant='contained' size='small' fullWidth={fullWidth} startIcon={<Plus />}>
+            Tambah Data Garansi
+          </Button>
+          <Button variant='contained' size='small' fullWidth={fullWidth} startIcon={<Import />}>
+            Import Data Garansi
+          </Button>
+          <Button variant='contained' size='small' fullWidth={fullWidth} startIcon={<Export />}>
+            Export Data Garansi
+          </Button>
+        </Stack>
       </Grid>
 
       <Grid item xs={12}>
