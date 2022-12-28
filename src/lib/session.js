7e0aac2c8095e5d23ec51a6b938c2c5e6fb01a4f
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-12-28 11:50:19 +0700

    Add and config iron-session

---
+++ src/lib/session.js
@@ -0,0 +1,19 @@
+import { withIronSessionApiRoute, withIronSessionSsr } from 'iron-session/next'
+
+const sessionOptions = {
+  password: process.env.COOKIE_PASSWORD,
+  cookieName: process.env.COOKIE_NAME,
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production'
+  }
+}
+
+const withSessionRoute = handler => {
+  return withIronSessionApiRoute(handler, sessionOptions)
+}
+
+const withSessionSsr = handler => {
+  return withIronSessionSsr(handler, sessionOptions)
+}
+
+export { withSessionRoute, withSessionSsr }
