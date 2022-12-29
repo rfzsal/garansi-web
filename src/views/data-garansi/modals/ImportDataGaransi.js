0fb0e84665f20a5de059490d77e9106a501758de
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-12-29 12:47:13 +0700

    Change dialog to modal

--- src/views/data-garansi/modals/ImportDataGaransi.js
+++ src/views/data-garansi/modals/ImportDataGaransi.js
@@ -1,19 +1,31 @@
 import { useRef, useState } from 'react'
+import Modal from '@mui/material/Modal'
+import Fade from '@mui/material/Fade'
+import Backdrop from '@mui/material/Backdrop'
 import Button from '@mui/material/Button'
-import Dialog from '@mui/material/Dialog'
-import DialogActions from '@mui/material/DialogActions'
-import DialogContent from '@mui/material/DialogContent'
-import DialogContentText from '@mui/material/DialogContentText'
-import DialogTitle from '@mui/material/DialogTitle'
 import Box from '@mui/material/Box'
 import { visuallyHidden } from '@mui/utils'
 import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+import Stack from '@mui/material/Stack'
 
 import Import from 'mdi-material-ui/Import'
 
 import { useModal } from 'src/hooks/useModal'
 import { exportFile, readFile } from 'src/lib/excel'
 import { useGaransi } from 'src/hooks/useGaransi'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  outline: 'none',
+  p: 4,
+  borderRadius: 1
+}
 
 const ImportDataGaransi = ({ open }) => {
   const { importData } = useGaransi()
@@ -54,33 +66,45 @@ const ImportDataGaransi = ({ open }) => {
 
   return (
     <>
-      <Dialog open={open} onClose={handleCloseModal} maxWidth='xs' fullWidth>
-        <DialogTitle>Import Data Garansi</DialogTitle>
+      <Modal
+        open={open}
+        onClose={handleCloseModal}
+        maxWidth='xs'
+        fullWidth
+        disableEnforceFocus
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography variant='h6' component='h2'>
+              Import Data Garansi
+            </Typography>
 
-        <DialogContent>
-          <DialogContentText>
-            Template import bisa diunduh{' '}
-            <Link onClick={handleGetTemplate} href='#'>
-              disini
-            </Link>
-          </DialogContentText>
+            <Typography variant='body2' component='p' marginTop={4} marginBottom={2}>
+              Template import bisa diunduh{' '}
+              <Link onClick={handleGetTemplate} href='#'>
+                disini
+              </Link>
+            </Typography>
 
-          <Box sx={{ mt: 2 }}>
-            <Box sx={visuallyHidden}>
-              <input type='file' ref={inputRef} accept='.xlsx, .xls' onChange={handleChangeFile} />
+            <Box sx={{ mt: 2 }}>
+              <Box sx={visuallyHidden}>
+                <input type='file' ref={inputRef} accept='.xlsx, .xls' onChange={handleChangeFile} />
+              </Box>
+
+              <Button onClick={clickFileInput} variant='outlined' fullWidth startIcon={file ? null : <Import />}>
+                {file ? file.name : 'Import Data'}
+              </Button>
             </Box>
 
-            <Button onClick={clickFileInput} variant='outlined' fullWidth startIcon={file ? null : <Import />}>
-              {file ? file.name : 'Import Data'}
-            </Button>
+            <Stack direction='row' justifyContent='flex-end' alignItems='center' spacing={2} marginTop={4}>
+              <Button onClick={handleCloseModal}>Batal</Button>
+              <Button onClick={handleImport}>Import</Button>
+            </Stack>
           </Box>
-        </DialogContent>
-
-        <DialogActions>
-          <Button onClick={handleCloseModal}>Batal</Button>
-          <Button onClick={handleImport}>Import</Button>
-        </DialogActions>
-      </Dialog>
+        </Fade>
+      </Modal>
     </>
   )
 }
