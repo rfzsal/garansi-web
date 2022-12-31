37f688b2fca79a88be184b3003061e2a3091249a
rfzsal <58117968+rfzsal@users.noreply.github.com>
2022-12-31 20:42:06 +0700

    fix ai

--- src/pages/api/klaim/index.js
+++ src/pages/api/klaim/index.js
@@ -7,8 +7,8 @@ const handler = async (req, res) => {
   const isAuth = user?.role === 'admin' || user?.role === 'distributor'
 
   const sql = isAuth
-    ? 'SELECT data_klaim.id, data_klaim.id_garansi, data_garansi.id, data_garansi.nama_produk, data_klaim.no_telepon, data_klaim.status, data_klaim.keterangan, UNIX_TIMESTAMP(data_klaim.tanggal_klaim) AS tanggal_klaim, UNIX_TIMESTAMP(data_garansi.tanggal_akhir) AS tanggal_akhir FROM data_klaim INNER JOIN data_garansi ON data_klaim.id_garansi = data_garansi.id'
-    : 'SELECT data_klaim.id, data_klaim.id_garansi, data_garansi.id, data_garansi.nama_produk, data_klaim.status, data_klaim.keterangan, UNIX_TIMESTAMP(data_klaim.tanggal_klaim) AS tanggal_klaim, UNIX_TIMESTAMP(data_garansi.tanggal_akhir) AS tanggal_akhir FROM data_klaim INNER JOIN data_garansi ON data_klaim.id_garansi = data_garansi.id'
+    ? 'SELECT data_klaim.id, data_klaim.id_garansi, data_garansi.nama_produk, data_klaim.no_telepon, data_klaim.status, data_klaim.keterangan, UNIX_TIMESTAMP(data_klaim.tanggal_klaim) AS tanggal_klaim, UNIX_TIMESTAMP(data_garansi.tanggal_akhir) AS tanggal_akhir FROM data_klaim INNER JOIN data_garansi ON data_klaim.id_garansi = data_garansi.id'
+    : 'SELECT data_klaim.id, data_klaim.id_garansi, data_garansi.nama_produk, data_klaim.status, data_klaim.keterangan, UNIX_TIMESTAMP(data_klaim.tanggal_klaim) AS tanggal_klaim, UNIX_TIMESTAMP(data_garansi.tanggal_akhir) AS tanggal_akhir FROM data_klaim INNER JOIN data_garansi ON data_klaim.id_garansi = data_garansi.id'
 
   const [data, error] = await query(sql)
 
