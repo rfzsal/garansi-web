1f962e4584541870655fe8963ce8ee5884a8300c
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-01 13:44:26 +0700

    update

--- src/pages/data-klaim/index.js
+++ src/pages/data-klaim/index.js
@@ -4,38 +4,23 @@ import Card from '@mui/material/Card'
 import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
 
-import Export from 'mdi-material-ui/Export'
 import Refresh from 'mdi-material-ui/Refresh'
 
 import { useKlaim } from 'src/hooks/useKlaim'
 import { withSessionSsr } from 'src/lib/session'
 import KlaimTable from 'src/views/data-klaim/KlaimTable'
 import { useModal } from 'src/hooks/useModal'
-import ExportDataGaransi from 'src/views/data-klaim/modals/ExportDataKlaim'
 
 const DataKlaim = () => {
   const { refresh } = useKlaim()
   const { openModal, modalOpened } = useModal()
   const fullWidth = useMediaQuery(theme => theme.breakpoints.down('lg'))
 
-  const handleOpenExportModal = () => {
-    openModal('ExportDataGaransi')
-  }
-
   return (
     <>
       <Grid container spacing={6}>
         <Grid item xs={12}>
           <Stack gap={2} direction={fullWidth ? 'column' : 'row'}>
-            <Button
-              onClick={handleOpenExportModal}
-              variant='contained'
-              size='small'
-              fullWidth={fullWidth}
-              startIcon={<Export />}
-            >
-              Export Data Klaim
-            </Button>
             <Button onClick={refresh} variant='contained' size='small' fullWidth={fullWidth} startIcon={<Refresh />}>
               Refresh Data Klaim
             </Button>
@@ -48,8 +33,6 @@ const DataKlaim = () => {
           </Card>
         </Grid>
       </Grid>
-
-      <ExportDataGaransi open={modalOpened === 'ExportDataGaransi'} />
     </>
   )
 }
