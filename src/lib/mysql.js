32adb60d081cef5bf6e442b094a21f9d478da6c1
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-15 23:32:03 +0700

    fix mysql

--- src/lib/mysql.js
+++ src/lib/mysql.js
@@ -1,19 +1,27 @@
 import mysql from 'mysql2'
 
-const db = mysql
-  .createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0
-  })
-  .promise()
+let pool = null
+
+const connect = () => {
+  if (pool) return pool
+
+  return mysql
+    .createPool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      waitForConnections: true,
+      connectionLimit: 10,
+      maxIdle: 10,
+      idleTimeout: 60000,
+      queueLimit: 0
+    })
+    .promise()
+}
+
+const db = connect()
 
 const query = sql => {
   return db
