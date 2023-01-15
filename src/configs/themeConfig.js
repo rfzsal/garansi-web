be84cc1bc308ccc54a78ef0d5e03b0b3bede45e9
rfzsal <58117968+rfzsal@users.noreply.github.com>
2023-01-15 23:50:08 +0700

    update theme

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -1,6 +1,6 @@
 const themeConfig = {
   // ** Layout Configs
-  templateName: 'Materio' /* App Name */,
+  templateName: 'PT CAHAYA DISTRIBUSI' /* App Name */,
   mode: 'light' /* light | dark */,
   contentWidth: 'boxed' /* full | boxed */,
   // ** Routing Configs
